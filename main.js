c40a44a588dc1cb693690b235972e66a18a47212
KianShah10385 <108907854+KianShah10385@users.noreply.github.com>
2022-12-29 20:33:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,14 +12,25 @@ function start()
          console.log(event);
          var Content = event.results[0][0].transcript;
           document.getElementById("textbox").innerHTML = Content;
-           console.log(Content); speak(); }
+           console.log(Content); 
+           if(Content =="take my selfie")
+           {
+            console.log("taking selfie --- ");
+            speak();
+           }
+         }
+
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
-    console.log("bye, Kian")
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
 }
 Webcam.set({
     width:360,
@@ -28,3 +39,17 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_url) {
+        document.getElementById("result").innerHTML = '<img id="selfie_img" src="'+data_uri;+'">';
+    });
+}
+function save()
+{
+    link = document.getElementById("link");
+    image=document.getElementById("selfie_img").src;
+    link.href = image;
+    link.click();
+}
